5b82e06d9a587a89d6ee709dd8efaf35baafcaac
秦凯 <qinkai@sinosoft.com.cn>
2019-03-25 21:46:35 +0800

    correct pom

--- src/main/resources/user.sql
+++ src/main/resources/user.sql
@@ -4,7 +4,7 @@ Navicat MySQL Data Transfer
 Source Server         : localhost
 Source Server Version : 50162
 Source Host           : localhost:3306
-Source Database       : ssm
+Source Database       : renren
 
 Target Server Type    : MYSQL
 Target Server Version : 50162
